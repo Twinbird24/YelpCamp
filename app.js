d09f9a63df55f3ad0236861fef984b2ca772f7e5
Attila Komaromi <attila.kom@gmail.com>
2017-01-07 01:47:36 +0000

    Updated dburl

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://yelpcamp_user:yelpcamp24@ds157258.mlab.com:57258/my_yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://yelpcamp_user:yelpcamp24@ds157258.mlab.com:57258/my_yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
