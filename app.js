76bc5603ed79b8b4ac7ffb9f1a461e9f7c04d09d
Attila Komaromi <attila.kom@gmail.com>
2017-01-07 01:56:39 +0000

    Added default databaseurl

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL);
-//mongoose.connect("mongodb://yelpcamp_user:yelpcamp24@ds157258.mlab.com:57258/my_yelpcamp");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp"
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
